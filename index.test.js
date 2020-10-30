920ae8546eb0f015d69332f7454c2124883b695d
LetsMelon <domi.m@outlook.com>
2020-10-30 22:26:16 +0100

    ENHANCED tests

--- index.test.js
+++ index.test.js
@@ -68,7 +68,7 @@ describe('addIdToArray', () => {
   });
   describe('Object with items', () => {
     it('Should return an array with objects', () => {
-      const result = f([{name: 'Jeff', age: 19}, {name: 'Maria', age: 20}]);
+      const result = f([{name: 'Jeff', age: 19}, {name: 'Maria', age: 20}]);      
       expect(isArray(result)).toBe(true);
       expect(result.length).toBe(2);
       expect(isObject(result[0])).toBe(true);
@@ -123,6 +123,76 @@ describe('addIdToArray', () => {
       });
       it('Should have the give start as first id', () => {
         const result = f([{name: 'Jeff', age: 19}, {name: 'Maria', age: 20}], [], 100);
+        expect(result[0].id).toBe(100);
+        expect(result[1].id).toBe(101);
+      });
+    });
+  });
+  describe('Nested array', () => {
+    it('Should return an array with objects', () => {
+      const result = f([['Jeff', 19], ['Maria', 20]], ['name', 'age']);
+      expect(isArray(result)).toBe(true);
+      expect(result.length).toBe(2);
+      expect(isObject(result[0])).toBe(true);
+    });
+    it('Should return objects with id', () => {
+      const result = f([['Jeff', 19], ['Maria', 20]], ['name', 'age']);
+      expect(result[0].id).toBeDefined();
+      expect(typeof result[0].id).toBe('number');
+      expect(result[1].id).toBeDefined();
+      expect(typeof result[1].id).toBe('number');
+    });
+    it('Should return objects with headers', () => {
+      const result = f([['Jeff', 19], ['Maria', 20]], ['name', 'age']);
+      result.map((item) => {
+        expect(item.id).toBeDefined();
+        expect(typeof item.id).toBe('number');
+
+        expect(item.name).toBeDefined();
+        expect(typeof item.name).toBe('string');
+
+        expect(item.age).toBeDefined();
+        expect(typeof item.age).toBe('number');
+      });
+    });
+    it('Should work with different datatypes', () => {
+      // String:
+      let result = f([['Jeff', 19], ['Maria', 20]], ['name', 'age']);
+      expect(typeof result[0].name).toBe('string');
+
+      // Number:
+      result = f([['Jeff', 19], ['Maria', 20]], ['name', 'age']);
+      expect(typeof result[0].age).toBe('number');
+
+      // Boolean:
+      result = f([[true, false, false]], ['song_01', 'song_02', 'song_03']);
+      expect(typeof result[0].song_01).toBe('boolean');
+
+      // Object:
+      result = f([[{name: 'Jeff', age: 19}, false], [{name: 'Maria', age: 20}, true]], ['person', 'has a car?']);
+      expect(isObject(result[0].person)).toBe(true);
+      expect(result[1].person.name).toBe('Maria');
+      expect(result[1].person.age).toBe(20);
+      expect(result[1]['has a car?']).toBe(true);
+
+      // Array:
+      // Just some random cars!
+      result = f([['Mercedes CLS 350d.', 'Tesla Model X']], ['jeff_cars']);
+      expect(isArray(result[0].jeff_cars)).toBe(true);
+    });
+    describe('Should have a valid id', () => {
+      it('Should begin to count at 1', () => {
+        const result = f([['Jeff', 19], ['Maria', 20]], ['name', 'age']);
+        expect(result[0].id).toBe(1);
+        expect(result[1].id).toBe(2);
+      });
+      it('Should have a always positive id', () => {
+        const result = f([['Jeff', 19], ['Maria', 20]], ['name', 'age'], -2);
+        expect(result[0].id).toBe(1);
+        expect(result[1].id).toBe(2);
+      });
+      it('Should have the give start as first id', () => {
+        const result = f([['Jeff', 19], ['Maria', 20]], ['name', 'age'], 100);
         expect(result[0].id).toBe(100);
         expect(result[1].id).toBe(101);
       });
