01b52cd9e8617a6ef9b25858ab4a4653ed22ec95
LetsMelon <domi.m@outlook.com>
2020-10-31 12:36:27 +0100

    ENHANCED funation parameters

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const isArray = Array.isArray;
 const isObject = require('isobject');
 
-module.exports = (arr, headers = [], start = 1) => {
-  let i = start - 1 < 0 ? 0 : start - 1;
-  return arr.map((item) => {
-    i += 1;
+module.exports = (arr, headers, start, increment_name, increment_step) => {
+  arr = isArray(arr) ? arr : [arr];
+  headers = headers === undefined ? [] : isArray(headers) ? headers : [headers];
+  start = (start === undefined || typeof start !== 'number') ? 0 : start;
+  increment_name = (increment_name === undefined || typeof increment_name !== 'string' || increment_name.length < 1) ? 'id' : increment_name;
+  increment_step = (increment_step === undefined || typeof increment_step !== 'number') ? 1 : increment_step;
 
-    const idObj = { id: i };
+  let i = start - increment_step < 0 ? 0 : start - increment_step;
+  return arr.map((item) => {
+    i += increment_step;
+
+    const idObj = {};
+    idObj[increment_name] = i;
 
     if (isArray(headers) && headers.length === 1) {
       const head = headers[0].toString();
