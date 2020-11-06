e02f0b96aaf0a31013fa4e13bf3f968b4e8a9293
LetsMelon <domi.m@outlook.com>
2020-11-06 15:55:43 +0100

    FIXED id value calculation

--- index.js
+++ index.js
@@ -36,14 +36,13 @@ const simple_id_function = (item, params) => {
 module.exports = (arr, headers, start, increment_name, increment_step, custom_id_function) => {
   arr = isArray(arr) ? arr : [arr];
   headers = headers === undefined ? [] : isArray(headers) ? headers : [headers];
-  start = (start === undefined || typeof start !== 'number') ? 0 : start;
+  start = (start === undefined || typeof start !== 'number' || start < 0) ? 1 : start;
   increment_name = (increment_name === undefined || typeof increment_name !== 'string' || increment_name.length < 1) ? 'id' : increment_name;
   increment_step = (increment_step === undefined || typeof increment_step !== 'number') ? 1 : increment_step;
   custom_id_function = (custom_id_function === undefined || typeof custom_id_function !== 'function') ? simple_id_function : custom_id_function;
 
-  let i = start - increment_step < 0 ? 0 : start - increment_step;
   return arr.map((item, index) => {
-    i += increment_step;
+    const i = start + index * increment_step;
 
     const params = {current_number: i, index, increment_name, increment_step, start};
     let idObj = custom_id_function(item, params);
